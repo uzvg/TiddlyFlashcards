dd03aa7acac0ca3e8759ee3569a36a33cb5ede18
uzvg <uzvg@qq.com>
2026-09-13 13:30:03 +0800

    register config dialog

--- __init__.py
+++ __init__.py
@@ -1,2 +1,5 @@
 from aqt import mw
 from aqt.qt import QAction
+from .config import open_config_dialog
+# Register config action (called once when the add-on loads)
+mw.addonManager.setConfigAction(__name__, open_config_dialog)
